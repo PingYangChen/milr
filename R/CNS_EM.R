dc49715dbe4c29d1970ee27efac5bb252ac1b8ce
ChingChuan-Chen <zw12356@gmail.com>
2016-01-25 21:00:14 +0800

    update EM algorithm

--- R/CNS_EM.R
+++ R/CNS_EM.R
@@ -7,9 +7,8 @@
 #' @param ID 123
 #' @return An list involves coefficients and fitted values.
 #' @examples
-#' testData <- DGP(50, 3, runif(sample(5:21, 1), -5, 5))
-#' a <- milr(testData$Z, testData$X, testData$ID)
-#' coef(a)
+#' # testData <- DGP(400, 10, runif(12, -5, 5))
+#' # a <- CBR_FS(testData$Z, testData$X, testData$ID)
 #' @importFrom numDeriv hessian
 #' @export
 CBR_FS <- function(Z, X, ID){
@@ -18,7 +17,7 @@ CBR_FS <- function(Z, X, ID){
   d <- names(table(ID))
   n.d <- length(d)
   
-  out <- summary(glm(A$Z~A$X-1, family=binomial(link="logit")))$coefficients
+  out <- summary(glm(Z~X-1, family=binomial(link="logit")))$coefficients
   temp.Lp <- out[,4]
   temp.L <- out[,1]
   #b <- temp.L
@@ -26,15 +25,14 @@ CBR_FS <- function(Z, X, ID){
   ## i: iteration for Fisher Scoring
   for(i in 1:20){
     # i2: iteration for experiment unit
-    P <- exp(X%*%b); P <- P/(1+P) 
+    P <- exp(X%*%b); P <- P/(1+P)
     Xbar <- matrix(0,n,p) 
     Pi <- rep(1, n)
     z <- rep(0, n)
     for(i2 in 1:n.d)
     {
       sel <- which(ID == d[i2])
-      n.sel <- length(sel)
-      for(j in 1:n.sel)
+      for(j in 1:length(sel))
       {
         Xbar[i2,] <- Xbar[i2,] + X[sel[j],]*P[sel[j]] 
         Pi[i2] <- Pi[i2]*(1 - 1/(1+exp(-sum(X[sel[j],]*b)))) 	
@@ -42,15 +40,15 @@ CBR_FS <- function(Z, X, ID){
       Pi[i2] <- 1 - Pi[i2]
       z[i2] <- Z[sel[1]]
     }
-    
     O <- diag((1-Pi)/Pi)
     D <- diag(1/Pi)
     V <- solve(t(Xbar)%*%O%*%Xbar)
     
     b.new <- b + V%*%t(Xbar)%*%D%*%(z-Pi)
-    test <- b.new - b; test <- sum(test*test)
+    sse <- crossprod(b.new - b)
     b <- b.new
-    if(test < 1e-6) break
+    if(sse < 1e-6)
+      break
   }
   
   test <- -abs(b/sqrt(diag(V)))
@@ -67,28 +65,18 @@ CBR_FS <- function(Z, X, ID){
 #' @param ID 123
 #' @return An list involves coefficients and fitted values.
 #' @examples
-#' testData <- DGP(50, 3, runif(sample(5:21, 1), -5, 5))
-#' a <- milr(testData$Z, testData$X, testData$ID)
-#' coef(a)
+#' # testData <- DGP(400, 10, runif(12, -5, 5))
+#' # a <- CBR_EM(testData$Z, testData$X, testData$ID)
 #' @export
 CBR_EM <- function(Z, X, ID){
   ## log-likelihood of z
   ## used for variance calculation
-  loglik <- function(b)
-  {
-    out <- 0
-    for(i in 1:n.d)
-    {
-      sel <- which(ID == d[i])
-      n.sel <- length(sel)
-      temp <- 1
-      for(j in 1:n.sel)
-        temp <- temp*(1 - 1/(1+exp(-sum(X[sel[j],]*b)))) 	
-      pii <- 1 - temp
-      out <- out + Z[sel[1]]*log(pii) + (1-Z[sel[1]])*log(1-pii) 
-      #cat(i, out,"\n")
-    }
-    return(out)
+  
+  loglik <- function(b,Z,X,ID){
+    pii <- split(as.data.frame(X), ID) %>% 
+      map(~1-prod(1-logit(as.matrix(.), b)))
+    return(map2_dbl(split(Z, ID) %>% map(unique), pii, 
+                    ~.x*log(.y)+(1-.x)*log(1-.y)) %>% sum)
   }
   
   ## solving logistic-like regression with MM algorithm
@@ -96,11 +84,12 @@ CBR_EM <- function(Z, X, ID){
   {
     for(i in 1:200)
     {
-      pv <- exp(X%*%b); pv <- pv/(1+pv)
-      b.new <- b + XXi%*%t(X)%*%(y - pv)
-      test <- b.new - b; test <- sum(test*test)
+      pv <- exp(X%*%b) %>% {./(1+.)}
+      b.new <- b + t(X) %*% (y - pv) / XXi
+      sse <- crossprod(b.new-b)
       b <- b.new
-      if(test < 10^(-6)) break
+      if(sse < 10^(-6))
+        break
     }
     if(i > 199) cat("Do not converge in the inner loop after 200 iteration!\n")
     return(b)
@@ -110,35 +99,33 @@ CBR_EM <- function(Z, X, ID){
   d <- names(table(ID))
   n.d <- length(d)
   
-  out <- summary(glm(A$Z~A$X-1, family=binomial(link="logit")))$coefficients
+  out <- summary(glm(Z~X-1, family=binomial(link="logit")))$coefficients
   temp.Lp <- out[,4]
   temp.L <- out[,1]
   b <- temp.L
-  
-  XXi <- solve(t(X)%*%X/4)
-  
+  XXi <- .25*(nrow(X)-1)
   # EM iterations
   for(i in 1:500)
   {
-    pv <- exp(X%*%b); pv <- pv/(1+pv)		
+    pv <- exp(X%*%b) %>% {./(1+.)}
     qv <- pv
-    for(i2 in 1:n.d){
+    for(i2 in 1:n.d)
+    {
       sel <- which(ID == d[i2])
-      n.sel <- length(sel)
       temp <- 1- prod(1-pv[sel])
-      qv[sel] <- qv[sel]/temp
+      qv[sel] <- qv[sel] / temp
     }
     y <- Z*qv
     b.new <- MMLogit(y, X, b)
     
-    test <- b.new-b; test <- sum(test*test)
+    sse <- crossprod(b.new-b)
     b <- b.new
-    if (test < 10^(-6)) break
+    if (sse < 10^(-6)) break
   }
   if (i > 499)
     cat("Do not converge after 500 iterations!\n")
   
-  b.var <- -diag(solve(hessian(loglik, b)))
+  b.var <- -diag(solve(hessian(function(b) loglik(b,Z,X,ID), b)))
   test <- -abs(b/sqrt(b.var))
   b.p <- 2*pnorm(test)
   return(list(NMLE=temp.L, NTest=temp.Lp, CMLE=b, CTest=b.p))

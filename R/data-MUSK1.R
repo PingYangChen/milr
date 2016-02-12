999878439f9a4fe301987c40d24df522187d3450
ChingChuan-Chen <zw12356@gmail.com>
2016-02-12 13:55:42 +0800

    update data description

--- R/data-MUSK1.R
+++ R/data-MUSK1.R
@@ -1,5 +1,10 @@
 #' Benchmark dataset for multiple instance experiments
 #' 
+#' The MUSK data sets are the most widely used benchmark data sets when comparing 
+#' different MI learning methods. The MUSK datasets consist of conformations. Each 
+#' conformation is represented by 166 features. In MUSK1, the average conformation
+#' in one bag is 5. For more detailed descriptions, please refer to the paper in 'Source'.
+#' 
 #' @format A data.frame with 476 observations on 168 variables.
 #' \itemize{
 #'   \item \code{bag}: bag id

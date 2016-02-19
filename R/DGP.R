ad6dd787ec0d354a4c1391f9f594c64beb0a7b31
ChingChuan-Chen <zw12356@gmail.com>
2016-02-19 10:17:59 +0800

    update DGP

--- R/DGP.R
+++ R/DGP.R
@@ -22,6 +22,7 @@ DGP <- function(n, m, beta){
     m <- rep(m, length = n)
   X <- scale(matrix(rnorm(sum(m)*p),sum(m),p)) %>% 
     matrix(nrow(.), ncol(.))  # remove attributes
+  X[ ,1] <- rep(1, nrow(X))
   Y <- logit(X, beta) %>% rbinom(sum(m), 1, .)
   ID <- rep(1:n, m)
   Z <- split(Y, ID) %>% map(~rep(any(. == 1), length(.))) %>% unlist %>% as.integer

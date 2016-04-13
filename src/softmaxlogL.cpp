f17584ce59c01cace2a7f9d635fb828b61e5f1ac
ChingChuan-Chen <zw12356@gmail.com>
2016-04-13 13:42:06 +0800

    minor bug fixed

--- src/softmaxlogL.cpp
+++ src/softmaxlogL.cpp
@@ -7,7 +7,7 @@ using namespace arma;
 
 // [[Rcpp::export]]
 double softmaxlogL(arma::vec bag, arma::mat instance, arma::vec label_bag, arma::vec beta, double alpha){
-  vec p_instance = pow(1.0 + exp(-X*beta), -1.0);
+  vec p_instance = pow(1.0 + exp(-instance * beta), -1.0);
   p_instance.elem(find(p_instance < 1e-7)).fill(1e-7);
   p_instance.elem(find(p_instance > 1-1e-7)).fill(1-1e-7);
   vec p_bag = zeros<vec>(label_bag.n_elem);

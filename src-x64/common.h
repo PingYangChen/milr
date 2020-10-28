707be48409b5601435f6bf294bf618f5608f00a4
PingYangChen <pychen.ping@gmail.com>
2020-10-28 15:49:27 +0800

    change version number back

--- src-x64/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef common_h
-#define common_h
-
-#include <RcppArmadillo.h>
-
-void chk_mat(const arma::mat& x, const std::string& varName);
-arma::vec logit(const arma::mat& X, const arma::vec& beta);
-#endif
